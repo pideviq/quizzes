1038b61aef4e9ef0f8ee3678e589e763fb4f00db
PI Dev Corp <pideviq@gmail.com>
2016-07-22 20:41:13 +0300

    refactor yandex_quiz_2.py

--- Python/yandex_quiz_2.py
+++ Python/yandex_quiz_2.py
@@ -10,6 +10,9 @@
 import re
 
 def checkLogin(login):
+    '''
+    Login validation function
+    '''
     if len(login) not in range(1, 21): return False
     if re.match(r'(^[A-Za-z]+[A-Za-z\d.-]*[A-Za-z\d]{1}$)', login):
         return True
@@ -18,18 +21,22 @@ def checkLogin(login):
 
 
 #===========================   Second variant   ===============================#
-letters = 'abcdefghijklmnopqrstuvwxyz'
-letters += letters.upper()
-numbers = '0123456789'
+from string import ascii_letters, digits
+
 signs   = '.-'
-allowed = letters + numbers + signs
+allowed = ascii_letters + digits + signs
 
 def checkLogin2(login):
-    if len(login) < 1 or len(login) > 20: return False
-    if (login[0] not in letters or login[-1] not in (letters + numbers)):
+    '''
+    Login validation function
+    '''
+    if len(login) not in range(1, 21): return False
+    if login[0] not in ascii_letters or login[-1] not in (ascii_letters + digits):
         return False
+
     for symbol in login[1:-1]:
         if symbol not in allowed: return False
+
     return True
 
 
@@ -39,11 +46,11 @@ if __name__ == '__main__':
     assert checkLogin('Apple-') == False, 'must end with latin letter or number'
     assert checkLogin('App$e') == False, 'should contain only latin letters, numbers, "." or "-"'
     assert checkLogin('') == False, 'login length must be at least 1 charackter'
-    assert checkLogin('abcdefghklmnopqrstuvwxyz') == False, 'login length mustn\'t be more than 20 charackters'
+    assert checkLogin('abcdefghklmnopqrstuvwxyz') == False, 'login length should be less than 20 charackters'
 
     assert checkLogin2('app1e') == True, 'checkLogin'
     assert checkLogin2('0e.g-g') == False, 'must start with latin letter'
     assert checkLogin2('Apple-') == False, 'must end with latin letter or number'
     assert checkLogin2('App$e') == False, 'should contain only latin letters, numbers, "." or "-"'
     assert checkLogin2('') == False, 'login length must be at least 1 charackter'
-    assert checkLogin2('abcdefghklmnopqrstuvwxyz') == False, 'login length mustn\'t be more than 20 charackters'
+    assert checkLogin2('abcdefghklmnopqrstuvwxyz') == False, 'login length should be less than 20 charackters'
